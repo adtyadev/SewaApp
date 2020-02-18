8df7e6098db8c0e9d4eb467de7e4aa41cf4c2931
gitKharisma01 <akharismaw77@gmail.com>
2020-02-18 21:55:09 +0700

    create footer tab navigation without render footer tab it self

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ import * as Font from 'expo-font';
 import { Ionicons } from '@expo/vector-icons';
 import Styles from './src/css/Styles';
 import Navigation from './src/pages/Navigation';
+import Example from './src/pages/example';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import SplashScreen from './src/pages/SplashScreen';
@@ -63,7 +64,8 @@ export default class App extends Component {
         {/* <Transaction/> */}
         {/* <Inbox /> */}
         {/* <Home/> */}
-        <Navigation />
+        {/* <Navigation /> */}
+        <Example/>
       </Container>
 
     );
