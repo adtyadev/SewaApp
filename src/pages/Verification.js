5043c2732701cb15c7b43689f39c182de7e57dec
gitKharisma01 <akharismaw77@gmail.com>
2020-03-16 15:11:46 +0700

    add modal info

--- src/pages/Verification.js
+++ src/pages/Verification.js
@@ -8,10 +8,19 @@ import {
 import { View, Dimensions, TouchableOpacity, StatusBar, Image } from 'react-native';
 import Styles from '../css/Styles';
 import { MaterialCommunityIcons, Ionicons } from 'react-native-vector-icons';
+import Modal from "react-native-modal";
 // import getTheme from '../native-base-theme/components';  
 // import styleTheme from '../native-base-theme/variables/platform.js';
+// const deviceHeight = (Dimensions.get('window').height)-300;
 export default class Verification extends Component {
+    
+    state = {
+        isModalVisible: false
+    };
 
+    toggleModal = () => {
+        this.setState({ isModalVisible: !this.state.isModalVisible });
+    };
     render() {
         return (
             <Container style={{ backgroundColor: "#23B0FF" }}>
@@ -28,9 +37,38 @@ export default class Verification extends Component {
                         <Title>Verification ID</Title>
                     </Body>
                     <Right style={{ marginRight: 0 }}>
-                        <Ionicons name="ios-information-circle-outline" color={"white"} size={22} />
+                        <TouchableOpacity
+                            onPress={this.toggleModal}
+                        >
+                            <Ionicons name="ios-information-circle-outline" color={"white"} size={22} />
+                        </TouchableOpacity>
+
                     </Right>
                 </Header>
+                <Modal 
+                isVisible={this.state.isModalVisible}
+                onBackdropPress={() => this.setState({ isModalVisible: false })}
+                onSwipeComplete={() => this.setState({ isModalVisible: false })}
+                onBackButtonPress={()=>this.setState({ isModalVisible: false })}
+                swipeThreshold={80}
+                swipeDirection="down"
+                >
+                    <View style={Styles.flex1ColumnCenter}>
+                    <View style={{ backgroundColor:"white", borderRadius:15}}>
+                    <Text note style={{ textAlign:"center",padding:10 }}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                    when an unknown printer took a galley of type and scrambled it to make a type specimen book.!
+                    </Text>
+                    <Button transparent onPress={this.toggleModal} style={{alignItem:"center", justifyContent:"center"}}>
+                    <Text>Close</Text>
+                    </Button>
+                    </View>
+                    
+                   
+
+                        
+                    </View>
+                </Modal>
                 <Container style={Styles.flex1Column}>
                     <View style={{
                         flex: 2, alignItems: "center",
@@ -50,12 +88,12 @@ export default class Verification extends Component {
                     </View>
                     <View style={{ flex: 1, alignItems: "center", justifyContent: "center", paddingHorizontal: 30, flexDirection: "column" }}>
                         <Button block style={Styles.buttonStyle}
-                        onPress={() => {  this.props.navigation.navigate('VerificationDocument') }}
+                            onPress={() => { this.props.navigation.navigate('VerificationDocument') }}
                         >
                             <Text>Verify</Text>
                         </Button>
-                        <TouchableOpacity 
-                        onPress={() => { this.props.navigation.goBack() }}
+                        <TouchableOpacity
+                            onPress={() => { this.props.navigation.goBack() }}
                         >
                             <Text note style={{ fontSize: 17, color: "#23B0FF", lineHeight: 30, textAlign: "center", paddingVertical: 10 }}>
                                 Don't have ID. Cancel Verification
